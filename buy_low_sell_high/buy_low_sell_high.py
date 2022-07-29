5835e0634e36f21c15e149f346df49d0347dd562
Kexin996 <kz2031@nyu.edu>
2022-07-29 14:40:56 -0400

    1

---
+++ buy_low_sell_high/buy_low_sell_high.py
@@ -0,0 +1,10 @@
+"""
+A simple trading strategy
+we will get the data by using DataReader.
+Documentation: https://pydata.github.io/pandas-datareader/
+"""
+
+
+import pandas_datareader as pdr
+import pandas as pd
+import datetime as dt
