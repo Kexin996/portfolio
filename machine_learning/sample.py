3d1f964d1b0ce60788bb7b3be3c0e7c7cfa5afa2
Kexin996 <kz2031@nyu.edu>
2022-08-03 14:31:55 -0400

    OLS

--- machine_learning/sample.py
+++ machine_learning/sample.py
@@ -23,17 +23,11 @@ def load_financial_data(s_date,e_date,name,output_file):
         
     return df
 
-# we load the data from 2015 to 2020
-# %%
-
-# we see the head of the data
-amzn_data = load_financial_data('2015-01-01','2020-01-01','AMZN','amzn.pkl')
-
-
 # %%
 
 # we define a simple classification signal for whether the price goes up or down
 def create_classification_trading_condition(df):
+    # it is no inplace, so we need to returnm a new dataframre
     df['Open-Close'] = df['Open']-df['Close']
     df['High-Low'] = df['High'] - df['Low']
     
@@ -46,32 +40,34 @@ def create_classification_trading_condition(df):
     # if so,y is 1
     # else, we use -1
     Y = np.where(df['Close'].shift(-1)>df['Close'], 1,-1)
+    df['Target'] = Y
     
-    return X,Y
+    return df,X,Y
 
-# %%
-X,Y = create_classification_trading_condition(amzn_data)
 
 # %%
 
 # we define a regression condition function
 def create_regression_trading_condition(df):
+    
     # the only difference is how we define y
-    df['Open-Close'] = df['Open']-df['Close']
+    df['Open-Close'] = df['Open']-df['Close'] 
+    # it is no inplace, so we need to returnm a new dataframre
     df['High-Low'] = df['High'] - df['Low']
-    df = df.dropna() 
+    df['Target'] = df['Close'].shift(-1)-df['Close']
+    df = df.dropna()
     X = df[['Open-Close','High-Low']]
     
     # if Y is positive, the close price goes up
     # if Y is negative, the close price goes down
     # if it is 0, the close price doesn't change
     # it catches both direction and magnitude
-    Y = df['Close'].shift(-1)-df['Close']
-    return X,Y
-# %%
-X,Y = create_regression_trading_condition(amzn_data)
-# we can see the last value is NaN, as we have shifted by -1
-print(Y)
+    # we can see the last value is NaN, as we have shifted by -1
+ 
+    Y = df['Target']
+    
+    return df,X,Y
+    
 
 # %%
 
