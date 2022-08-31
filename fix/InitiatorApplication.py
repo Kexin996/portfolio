c36ff0e90786d2fd1e3b9856b61a9ba1dbbb550e
Kexin996 <kz2031@nyu.edu>
2022-08-30 23:07:03 -0400

    8.30

--- fix/InitiatorApplication.py
+++ fix/InitiatorApplication.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 29 15:27:26 2022
-
-@author: zhangkexin
+Implementation of initiator application class
 """
 
+import quickfix as fix 
+
+
+# inherit interface Application from base.py
+
+class Initiator(fix.Application):
+    
+    # create a logger
+    def create_logger(self,logger):
+        self.logger = logger
+
+    def onCreate(self,sessionID):
+        self.logger.info("we have created a seesion:",sessionID)
+        return
+    
+    def onLogon(self,sessionID):
+        self.logger.info("succesfully log on:",sessionID)
+        return
+    
+    def onLogout(self,sessionID):
+        self.logger.info('succesfully log out',sessionID)
+        return
+    
+    def fromAdmin(self,sessionID):
+        self.logger.info('Received:',sessionID)
+        return
+    
+    def toApp(self,sessionID):
+        self.logger.info('callback:',sessionID)
+        return
+     
+         
+        
+dir(fix)
